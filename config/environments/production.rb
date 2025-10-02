954a076c786c7c9bc4d7fdca254e3c9f242c2a54
emmant-web <emmanueldtoribio@gmail.com>
2025-10-02 16:18:24 +0800

    email confirmation fix attempt 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -111,4 +111,15 @@ Rails.application.configure do
     protocol: "https"
   }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              "smtp.gmail.com",
+  port:                 587,
+  domain:               "gmail.com",
+  user_name:            ENV["SMTP_USERNAME"],   # your Gmail address
+  password:             ENV["SMTP_PASSWORD"],   # your Gmail app password (NOT your regular Gmail password)
+  authentication:       "plain",
+  enable_starttls_auto: true
+  }
+
 end
